f895e15022230b463c6ee336eb27db69201dcf73
Linaly1018 <miyamoto.linaly@gmail.com>
2018-07-30 15:59:26 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -4,8 +4,8 @@ word = "Hello"
 second_word = "Friends"
 
 def crazy_strings(word, second_word)
-  return word = word.upcase.reverse
-  return second_word = second_word.swapcase
+  word = word.upcase.reverse
+  second_word = second_word.swapcase
 end
 
 puts crazy_strings(word, second_word)
